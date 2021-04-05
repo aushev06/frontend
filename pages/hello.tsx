c02401a93aecca85e4e72e228f22c70115e03910
Adam Doskhoev <Adam.Doskhoev@glidewelldental.com>
2021-04-05 22:05:36 +0300

    fix: add LikeBlock to hello page

--- pages/hello.tsx
+++ pages/hello.tsx
@@ -1,3 +1,31 @@
+import { useState } from 'react';
+import { LikeBlock } from '../components/LikeBlock';
+
 export default function HelloPage() {
-  return <h1>Hello, World!!!</h1>;
+  /* REMOVE THIS ↓ */
+  const [vote, setVote] = useState(undefined);
+  const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
+  const onChange = (vote: 'like' | 'dislike') => {
+    // вызвать апи, возвращающий кол-во лайков/дизлайков
+    const dataFromServer = {
+      vote: ['like', 'dislike'][Math.floor(2 * Math.random())],
+      likes: Math.floor(100 * Math.random()),
+      dislikes: Math.floor(100 * Math.random()),
+    };
+    setVote(dataFromServer.vote);
+    setLikes(dataFromServer.likes);
+    setDislikes(dataFromServer.dislikes);
+  };
+
+  return (
+    <>
+      <div className={'p-30'}>
+        <LikeBlock mode={'full'} likes={likes} dislikes={dislikes} vote={vote} onChange={onChange} />
+      </div>
+      <div className={'p-30'}>
+        <LikeBlock mode={'mini'} likes={likes} dislikes={dislikes} vote={vote} onChange={onChange} />
+      </div>
+    </>
+  );
 }
