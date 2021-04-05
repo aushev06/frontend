44f947504ae2e1d5469c3b7d7315d17b00aac07d
Adam Doskhoev <Adam.Doskhoev@glidewelldental.com>
2021-04-05 20:48:23 +0300

    fix: code review little fixes

--- components/LikeBlock/LikeBlock.tsx
+++ components/LikeBlock/LikeBlock.tsx
@@ -11,10 +11,7 @@ interface LikeBlockProps {
   onChange: (vote: 'like' | 'dislike') => void;
 }
 
-export const LikeBlock: React.FC<LikeBlockProps> = ({ mode, likes: upCount, dislikes: downCount, vote, onChange }) => {
-  const thumbUpClassName = vote === 'like' ? styles.likeVoted : styles.noVotes;
-  const thumbDownClassName = vote === 'dislike' ? styles.dislikeVoted : styles.noVotes;
-
+export const LikeBlock: React.FC<LikeBlockProps> = ({ mode, likes, dislikes, vote, onChange }) => {
   const onThumbUpClick = () => {
     onChange('like');
   };
@@ -22,16 +19,30 @@ export const LikeBlock: React.FC<LikeBlockProps> = ({ mode, likes: upCount, disl
     onChange('dislike');
   };
 
+  const {
+    likeVoted,
+    dislikeVoted,
+    noVotes,
+    likeBlock,
+    likeBlockFull,
+    likeBlockMini,
+    upBlock,
+    downBlock,
+    rotate,
+  } = styles;
+  const thumbUpClassName = vote === 'like' ? likeVoted : noVotes;
+  const thumbDownClassName = vote === 'dislike' ? dislikeVoted : noVotes;
+
   return (
-    <div className={styles.likeBlock}>
-      <div className={mode === 'full' ? styles.likeBlockFull : styles.likeBlockMini}>
-        <div className={clsx(styles.upBlock, thumbUpClassName)} onClick={onThumbUpClick}>
+    <div className={likeBlock}>
+      <div className={mode === 'full' ? likeBlockFull : likeBlockMini}>
+        <div className={clsx(upBlock, thumbUpClassName)} onClick={onThumbUpClick}>
           <ThumbUpIcon className={thumbUpClassName} />
-          <span>{upCount}</span>
+          <span>{likes}</span>
         </div>
-        <div className={clsx(styles.downBlock, thumbDownClassName)} onClick={onThumbDownClick}>
-          <ThumbUpIcon className={clsx(thumbDownClassName, styles.rotate)} />
-          <span>{downCount}</span>
+        <div className={clsx(downBlock, thumbDownClassName)} onClick={onThumbDownClick}>
+          <ThumbUpIcon className={clsx(thumbDownClassName, rotate)} />
+          <span>{dislikes}</span>
         </div>
       </div>
     </div>
