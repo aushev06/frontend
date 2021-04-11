ee543ff72ae9a6c844a9c1391abb98ff5ffe7ceb
Movsar Khalakhoev <movsar.kh.128@gmail.com>
2021-04-11 17:45:14 +0300

    #5 Реализовано отображение времени, прошедшего с момента публикации поста

--- components/MiniPost/index.tsx
+++ components/MiniPost/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
+import { formatDistanceToNow } from 'date-fns';
+import { ru } from 'date-fns/locale';
 import { CommentIcon } from '../icons/CommentIcon';
 import { EyeIcon } from '../icons/EyeIcon';
 import { ILikeBlockResult, LikeBlock, LikeBlockVote } from '../LikeBlock';
@@ -42,15 +44,11 @@ const MiniPost: React.FC<MiniPostProps> = ({ postData }) => {
     dislikesCount,
     slug,
   } = postData;
-  const [likeOrDislikeVote, setLikeOrDislikeVote] = React.useState<LikeBlockVote>();
-
-  const getTimeLabel = (time: Date): string => {
-    return time.toLocaleString();
-  };
-
-  function setLikeOrDislikeHandler(vote: ILikeBlockResult) {
-    setLikeOrDislikeVote(vote.vote);
-  }
+  const [likesAndDislikes, setLikesAndDislikes] = React.useState<ILikeBlockResult>({
+    likes: likesCount,
+    dislikes: dislikesCount,
+    vote: undefined,
+  });
 
   return (
     <div className={styles.post}>
@@ -61,7 +59,7 @@ const MiniPost: React.FC<MiniPostProps> = ({ postData }) => {
             <span className={styles.user_name}>{user.name}</span>
           </div>
         </Link>
-        <span className={styles.time}>9 часов назад</span>
+        <span className={styles.time}>{formatDistanceToNow(time, { locale: ru, addSuffix: true })}</span>
         <div className={styles.tags}>
           {tags.map((tag) => (
             <span className={styles.tag}>#{tag}</span>
@@ -86,11 +84,11 @@ const MiniPost: React.FC<MiniPostProps> = ({ postData }) => {
         </div>
         <div className={styles.popularity_right}>
           <LikeBlock
-            vote={likeOrDislikeVote}
-            likes={likesCount}
-            dislikes={dislikesCount}
+            vote={likesAndDislikes.vote}
+            likes={likesAndDislikes.likes}
+            dislikes={likesAndDislikes.dislikes}
             mode="full"
-            onChange={setLikeOrDislikeHandler}
+            onChange={setLikesAndDislikes}
           />
         </div>
       </div>
