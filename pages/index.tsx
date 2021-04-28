d5464f69c199ee0a376ca8721da860f1901878f0
Movsar Khalakhoev <movsar.kh.128@gmail.com>
2021-04-28 12:49:12 +0300

    #35 Merge branch 'main' into Post

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { MainLayout } from '../layouts/MainLayout';
 import { SideComments } from '../components/SideComments';
 import { MiniPost, MiniPostData } from '../components/MiniPost';
-import { LeftCategoriesSide } from '../components/LeftCategoriesSide';
+import { SideBlock } from '../components/SideBlock';
+import { Tags } from '../components/Tags';
+import { MenuList } from '../components/MenuList';
+import { RecommendationsContainer, RecommendationItem, RecommendationItemData } from '../components/Recommendations';
 
 const miniPostTemplate: MiniPostData = {
   id: 0,
@@ -24,13 +27,99 @@ const miniPostTemplate: MiniPostData = {
   dislikesCount: 12,
 };
 
+const arrRecommendations: Array<RecommendationItemData> = [
+  {
+    id: 0,
+    title: 'В Магасе прошел Meetup по программированию и безопасности GDG Magas 2019',
+    user: {
+      id: 0,
+      name: 'Dr. Who',
+      avatarUrl: 'https://clck.ru/UDyci',
+    },
+    slug: 'mini-post',
+    time: 1618828849922,
+    imageUrl: 'https://clck.ru/U7rDJ',
+    commentsCount: 21,
+    viewsCount: 234,
+  },
+  {
+    id: 1,
+    title: 'Что бы поесть на ночь?',
+    user: {
+      id: 0,
+      name: 'Dr. Who',
+      avatarUrl: 'https://clck.ru/UDyci',
+    },
+    slug: 'mini-post',
+    time: 1618528849922,
+    imageUrl: 'https://clck.ru/U7rDJ',
+    commentsCount: 21,
+    viewsCount: 234,
+  },
+  {
+    id: 2,
+    title: 'Фэнтези в Ингушетии или в поисках потерянного архива',
+    user: {
+      id: 0,
+      name: 'Dr. Who',
+      avatarUrl: 'https://clck.ru/UDyci',
+    },
+    slug: 'mini-post',
+    time: 1618238849922,
+    imageUrl: 'https://clck.ru/U7rDJ',
+    commentsCount: 21,
+    viewsCount: 234,
+  },
+];
+
 export default function Home() {
   return (
     <main>
       <MainLayout>
         <div className="wrapper">
-          <LeftCategoriesSide />
-          <div className="content">{[...Array(10)].fill(<MiniPost postData={miniPostTemplate} />)}</div>
+          <div className="left-side">
+            <SideBlock>
+              <MenuList
+                items={[
+                  { name: 'Популярное', url: '/popular', icon: '/fire.svg', isActive: true, hasMore: true },
+                  { name: 'Новое', url: '/new', icon: '/news.svg' },
+                ]}
+              />
+            </SideBlock>
+            <SideBlock name="Подкасты">
+              <MenuList
+                items={[
+                  { name: 'Dev Battle', url: '/tags/dev-battle', icon: '/podcast_1.png' },
+                  { name: 'Design Battle', url: '/tags/dev-battle', icon: '/podcast_2.png' },
+                  { name: 'Design Review', url: '/tags/dev-battle', icon: '/podcast_3.png' },
+                  { name: 'Dev Review', url: '/tags/dev-battle', icon: '/podcast_4.png' },
+                  { name: 'Дизайн за чаем', url: '/tags/dev-battle', icon: '/podcast_5.png' },
+                ]}
+              />
+            </SideBlock>
+            <SideBlock name="Темы">
+              <Tags
+                items={[
+                  { name: 'разработка', url: '/tags/develop' },
+                  { name: 'советы', url: '/tags/tips' },
+                  { name: 'дизайн', url: '/tags/design' },
+                  { name: 'инструкции', url: '/tags/instructions' },
+                  { name: 'mvp', url: '/tags/mvp' },
+                  { name: 'проектирование', url: '/tags/test' },
+                  { name: 'инструментарий', url: '/tags/test' },
+                  { name: 'болтология', url: '/tags/test' },
+                  { name: 'работа', url: '/tags/test' },
+                ]}
+              />
+            </SideBlock>
+          </div>
+          <div className="content">
+            <RecommendationsContainer>
+              {arrRecommendations.length &&
+                arrRecommendations.map((item) => <RecommendationItem key={item.id} data={item} />)}
+            </RecommendationsContainer>
+            {[...Array(10)].fill(<MiniPost postData={miniPostTemplate} />)}
+          </div>
           <div className="right-side">
             <SideComments
               comments={[
