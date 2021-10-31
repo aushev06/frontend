7928dbbe05ba936116dce436e5761a6f0506ba6a
aushev06 <34713423+aushev06@users.noreply.github.com>
2021-11-01 02:30:44 +0300

    fix mobile

--- components/FullPost/index.tsx
+++ components/FullPost/index.tsx
@@ -14,8 +14,10 @@ import { LikeBlockResult } from '../LikeBlock';
 import getBlockHTML, { Block } from './PostViewer';
 import { WarningIcon } from '../icons/WarningIcon';
 import {PostData} from "../../interfaces";
+import {useAllMQ} from "../../utils/useAllMQ";
 
 export const FullPost: React.FC<{post: PostData, onSetLike: (postId: number, like: unknown) => void}> = ({post, onSetLike}) => {
+  const mq = useAllMQ();
   const [postData, setPostData] = React.useState(post);
   const [anchorEl, setAnchorEl] = React.useState<HTMLDivElement | null>(null);
   console.log(postData)
@@ -49,56 +51,58 @@ export const FullPost: React.FC<{post: PostData, onSetLike: (postId: number, lik
           <h1 className={styles.postHeader}>{post.title}</h1>
           <ViewsAndComments comments={post.comments_count} views={post.views} mode="full" />
         </div>
-        <div className={styles.postActions}>
-          <LikeBlock
-            likes={likesAndDislikes.likes}
-            dislikes={likesAndDislikes.dislikes}
-            mode="mini"
-            onChange={handleSetLike}
-            vote={likesAndDislikes.vote}
-          />
-          <div className={styles.postShare} onClick={handleClick}>
-            <ShareIcon className={styles.postShareIcon} />
-            Поделиться
-          </div>
-          <Popover
-            open={!!anchorEl}
-            anchorEl={anchorEl}
-            onClose={() => setAnchorEl(null)}
-            classes={{
-              paper: styles.postSharePopup,
-            }}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'center',
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'center',
-            }}
-          >
-            <div className={styles.postShareSocial}>
-              <FacebookIcon className={styles.postShareSocialIcon} />
-              Facebook
+        {!mq.isXS && (
+            <div className={styles.postActions}>
+              <LikeBlock
+                  likes={likesAndDislikes.likes}
+                  dislikes={likesAndDislikes.dislikes}
+                  mode="mini"
+                  onChange={handleSetLike}
+                  vote={likesAndDislikes.vote}
+              />
+              <div className={styles.postShare} onClick={handleClick}>
+                <ShareIcon className={styles.postShareIcon} />
+                Поделиться
+              </div>
+              <Popover
+                  open={!!anchorEl}
+                  anchorEl={anchorEl}
+                  onClose={() => setAnchorEl(null)}
+                  classes={{
+                    paper: styles.postSharePopup,
+                  }}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                  }}
+              >
+                <div className={styles.postShareSocial}>
+                  <FacebookIcon className={styles.postShareSocialIcon} />
+                  Facebook
+                </div>
+                <div className={styles.postShareSocial}>
+                  <VkIcon className={styles.postShareSocialIcon} />
+                  Вконтакте
+                </div>
+                <div className={styles.postShareSocial}>
+                  <TwitterIcon className={styles.postShareSocialIcon} />
+                  Twitter
+                </div>
+                <div className={styles.postShareSocial}>
+                  <TelegramIcon className={styles.postShareSocialIcon} />
+                  Telegram
+                </div>
+                <div className={styles.postShareSocial}>
+                  <WhatsappIcon className={styles.postShareSocialIcon} />
+                  Whatsapp
+                </div>
+              </Popover>
             </div>
-            <div className={styles.postShareSocial}>
-              <VkIcon className={styles.postShareSocialIcon} />
-              Вконтакте
-            </div>
-            <div className={styles.postShareSocial}>
-              <TwitterIcon className={styles.postShareSocialIcon} />
-              Twitter
-            </div>
-            <div className={styles.postShareSocial}>
-              <TelegramIcon className={styles.postShareSocialIcon} />
-              Telegram
-            </div>
-            <div className={styles.postShareSocial}>
-              <WhatsappIcon className={styles.postShareSocialIcon} />
-              Whatsapp
-            </div>
-          </Popover>
-        </div>
+        )}
       </div>
       {postData.body.map((block: Block, idx) => (<div key={'block-' + idx}>
         {getBlockHTML(block)}
