179af5f35821cb7e15b3b397831e1735c6b49513
Adam Balkoev <adam22185515@gmail.com>
2021-11-09 18:42:58 +0300

    Доверстал AuthDialog

--- components/AuthDialog/index.tsx
+++ components/AuthDialog/index.tsx
@@ -5,68 +5,79 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import CloseIcon from '@material-ui/icons/Close';
 import React from 'react';
 import Link from 'next/link';
+import IconButton from '@mui/material/IconButton';
 
 import styles from './AuthDialog.module.scss';
-import {DialogContentText, Typography} from "@material-ui/core";
-import {API_URL} from "../../core/axios";
+import { DialogContentText, Typography } from '@material-ui/core';
+import { API_URL } from '../../core/axios';
 
 interface ModalProps {
-    open?: boolean;
-    size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | false;
-    onClose: () => void;
-    className?: string;
+  open?: boolean;
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | false;
+  onClose: () => void;
+  className?: string;
 }
 
-export const AuthDialog: React.FC<ModalProps> = (
-    {
-        children,
-        open = false,
-        size = 'xs',
-        onClose,
-        className,
-    }
-): React.ReactElement => {
-    return (
-        <Dialog
-            maxWidth={'xs'}
-            open={open}
-            onClose={onClose}
-            fullWidth
-            PaperProps={{
-                className: styles.modal
-            }}
-        >
+export const AuthDialog: React.FC<ModalProps> = ({
+  children,
+  open = false,
+  size = 'xs',
+  onClose,
+  className,
+}): React.ReactElement => {
+  return (
+    <Dialog
+      maxWidth={'xs'}
+      open={open}
+      onClose={onClose}
+      fullWidth
+      PaperProps={{
+        className: styles.modal,
+      }}
+    >
+      <IconButton
+        aria-label="close"
+        onClick={onClose}
+        sx={{
+          color: (theme) => theme.palette.grey[500],
+        }}
+        style={{ alignSelf: 'flex-end', marginTop: 22, marginRight: 20, padding: 0 }}
+      >
+        <CloseIcon style={{ fontSize: 28 }} />
+      </IconButton>
 
-            <Button onClick={onClose}>
-                <CloseIcon style={{fontSize: 28}}/>
-            </Button>
+      <DialogContent style={{ paddingTop: 14, paddingBottom: 50 }}>
+        <DialogContentText className="mb-0">
+          <div className={styles.content}>
+            <Typography className={styles.title}> Добро пожаловать </Typography>
+            <Typography className={styles.subTitle}> Рады видеть вас снова! </Typography>
 
-            <DialogContent>
-                <DialogContentText>
-                    <div className={styles.content}>
-
-                        <Typography className={styles.title}> Добро пожаловать </Typography>
-                        <Typography className={styles.subTitle}> Рады видеть вас снова! </Typography>
-
-                        <Link href={`${API_URL}/social/vk`}>
-                            <a>
-                                <Button fullWidth={true} variant={'outlined'}>
-                                    <img src="/vk.png" alt=""/>
-                                    Войти через VK
-                                </Button>
-                            </a>
-                        </Link>
-                        <Link href={`${API_URL}/social/google`}>
-                            <a>
-                                <Button fullWidth={true} variant={'outlined'}>
-                                    <img src="/google.png" alt=""/>
-                                    Войти через Google
-                                </Button>
-                            </a>
-                        </Link>
-                    </div>
-                </DialogContentText>
-            </DialogContent>
-        </Dialog>
-    );
+            <Link href={`${API_URL}/social/vk`}>
+              <a>
+                <Button fullWidth={true} variant={'outlined'}>
+                  <img src="/vk.png" alt="" />
+                  Войти через VK
+                </Button>
+              </a>
+            </Link>
+            <Link href={`${API_URL}/social/google`}>
+              <a>
+                <Button fullWidth={true} variant={'outlined'}>
+                  <img src="/google.png" alt="" />
+                  Войти через Google
+                </Button>
+              </a>
+            </Link>
+          </div>
+          <div className="d-flex justify-content-center" style={{ marginTop: 40 }}>
+            <Typography className={styles.text}>
+              <span>
+                или <a className={styles.link}>создать аккаунт</a>
+              </span>
+            </Typography>
+          </div>
+        </DialogContentText>
+      </DialogContent>
+    </Dialog>
+  );
 };
